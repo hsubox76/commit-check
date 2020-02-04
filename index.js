c14de7c24d492e78dbe26d9d310fc312876a269a
Christina Holland <hsubox@gmail.com>
2020-02-04 15:09:36 -0800

    More logging

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ try {
       github.context.payload.pull_request &&
       github.context.payload.pull_request.title;
     if (prTitle) {
+      console.log(`PR Title: ${prTitle}`); 
       const match = prTitle.match(/^\[(feat|fix)\] (.+)/);
       if (!match) {
         throw Error('Fix PR title syntax');
+      } else {
+        core.info('Good title!');
       }
     }
   }
